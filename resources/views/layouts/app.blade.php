5088a534a92cdce4f2cc9514256acc93d2e6f7bb
“faizanrajput1120” <Faizanrajput488@gmail.com>
2026-04-21 02:38:00 +0500

    asas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,11 @@
-<!-- condition.blade.php -->
-@dump(auth()->user())
-@if (auth()->check() && auth()->user()->is_admin == 1)
-    @include('layouts.condition') <!-- Admin layout -->
-@elseif (auth()->check() && auth()->user()->is_admin == 3)
-    @include('layouts.user_layout') <!-- User layout -->
+@php
+    $user = auth()->user();
+@endphp
+
+@if ($user)
+    @if ($user->is_admin == 1)
+        @include('layouts.condition') {{-- Admin layout --}}
+    @elseif ($user->is_admin == 3)
+        @include('layouts.user_layout') {{-- User layout --}}
+    @endif
 @endif
